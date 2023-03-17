f7d315c2c8e204f9f0fe4a169b8c1b7a78c008ad
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2023-03-17 16:55:03 +0100

    Store entities as JSON and start dlite parse strat

---
+++ oteapi_optimade/dlite/parse.py
@@ -0,0 +1,178 @@
+"""OTEAPI strategy for parsing OPTIMADE structure resources to DLite instances."""
+import logging
+import sys
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import dlite
+from optimade.adapters import Structure
+from optimade.models import StructureResponseMany, StructureResponseOne
+from oteapi.models import SessionUpdate
+from oteapi_dlite.models import DLiteSessionUpdate
+from oteapi_dlite.utils import get_collection
+from pydantic import ValidationError
+from pydantic.dataclasses import dataclass
+
+from oteapi_optimade.exceptions import OPTIMADEParseError
+from oteapi_optimade.models import OPTIMADEParseConfig, OPTIMADEParseSession
+from oteapi_optimade.strategies.parse import OPTIMADEParseStrategy
+
+if TYPE_CHECKING:  # pragma: no cover
+    from typing import Any, Dict, Optional, Union
+
+
+LOGGER = logging.getLogger("oteapi_optimade.dlite")
+LOGGER.setLevel(logging.DEBUG)
+LOGGER.addHandler(logging.StreamHandler(sys.stdout))
+
+
+@dataclass
+class OPTIMADEDLiteParseStrategy:
+    """Parse strategy for JSON.
+
+    **Implements strategies**:
+
+    - `("mediaType", "application/vnd.optimade+dlite")`
+    - `("mediaType", "application/vnd.OPTIMADE+dlite")`
+    - `("mediaType", "application/vnd.OPTiMaDe+dlite")`
+    - `("mediaType", "application/vnd.optimade+DLite")`
+    - `("mediaType", "application/vnd.OPTIMADE+DLite")`
+    - `("mediaType", "application/vnd.OPTiMaDe+DLite")`
+
+    """
+
+    parse_config: OPTIMADEParseConfig
+
+    def initialize(
+        self, session: "Optional[Dict[str, Any]]" = None
+    ) -> DLiteSessionUpdate:
+        """Initialize strategy.
+
+        This method will be called through the `/initialize` endpoint of the OTE-API
+        Services.
+
+        Parameters:
+            session: A session-specific dictionary context.
+
+        Returns:
+            An update model of key/value-pairs to be stored in the session-specific
+            context from services.
+
+        """
+        return DLiteSessionUpdate(collection_id=get_collection(session).uuid)
+
+    def get(
+        self, session: "Optional[Union[SessionUpdate, Dict[str, Any]]]" = None
+    ) -> OPTIMADEParseSession:
+        """Request and parse an OPTIMADE response using OPT.
+
+        This method will be called through the strategy-specific endpoint of the
+        OTE-API Services.
+
+        Configuration values provided in `resource_config.configuration` take
+        precedence over the derived values from `downloadUrl`.
+
+        Workflow:
+
+        1. Request OPTIMADE response.
+        2. Parse as an OPTIMADE Python tools (OPT) pydantic response model.
+
+        Parameters:
+            session: A session-specific dictionary-like context.
+
+        Returns:
+            An update model of key/value-pairs to be stored in the session-specific
+            context from services.
+
+        """
+        session = OPTIMADEParseStrategy(self.parse_config).get(session)
+
+        entities_path = Path(__file__).resolve().parent.resolve() / "entities"
+
+        dlite.storage_path.append(str(entities_path / "*.json"))
+
+        JSONAPIResourceLinks = dlite.Instance.from_url(
+            f"json://{entities_path}/JSONAPIResourceLinks.json"
+        )
+        OPTIMADEStructure = dlite.Instance.from_url(
+            f"json://{entities_path}/OPTIMADEStructure.json"
+        )
+        OPTIMADEStructureAssembly = dlite.Instance.from_url(
+            f"json://{entities_path}/OPTIMADEStructureAssembly.json"
+        )
+        OPTIMADEStructureAttributes = dlite.Instance.from_url(
+            f"json://{entities_path}/OPTIMADEStructureAttributes.json"
+        )
+        OPTIMADEStructureSpecies = dlite.Instance.from_url(
+            f"json://{entities_path}/OPTIMADEStructureSpecies.json"
+        )
+
+        dlite_collection = get_collection(session)
+
+        if self.parse_config.configuration.return_object:
+            if "optimade_response_object" not in session:
+                raise ValueError(
+                    "'optimade_response_object' was expected to be present in the session."
+                )
+
+            # Currently, only structures entries are supported and handled
+            if isinstance(session.optimade_response_object, StructureResponseMany):
+                structures = [
+                    Structure(entry)
+                    if isinstance(entry, dict)
+                    else Structure(entry.dict())
+                    for entry in session.optimade_response_object.data
+                ]
+            elif isinstance(session.optimade_response_object, StructureResponseOne):
+                structures = [
+                    Structure(session.optimade_response_object.data)
+                    if isinstance(session.optimade_response_object.data, dict)
+                    else Structure(session.optimade_response_object.data.dict())
+                ]
+            else:
+                LOGGER.debug(
+                    "Got currently unsupported response type %s. Only structures are supported.",
+                    session.optimade_response_object.__class__.__name__,
+                )
+                raise OPTIMADEParseError(
+                    "The DLite OPTIMADE Parser currently only supports structures entities."
+                )
+        else:
+            if "optimade_response" not in session:
+                raise ValueError(
+                    "'optimade_response' was expected to be present in the session."
+                )
+
+            if not "data" in session.optimade_response:
+                LOGGER.debug("Not a successful response - no 'data' entry found.")
+                return session
+
+            if isinstance(session.optimade_response["data"], list):
+                try:
+                    structures = [
+                        Structure(entry) for entry in session.optimade_response["data"]
+                    ]
+                except ValidationError:
+                    LOGGER.debug(
+                        "Could not parse list of 'data' entries as structures."
+                    )
+                    raise OPTIMADEParseError(
+                        "The DLite OPTIMADE Parser currently only supports structures entities."
+                    )
+            elif session.optimade_response is not None:
+                try:
+                    structures = [Structure(session.optimade_response["data"])]
+                except ValidationError:
+                    LOGGER.debug("Could not parse single 'data' entry as a structure.")
+                    raise OPTIMADEParseError(
+                        "The DLite OPTIMADE Parser currently only supports structures entities."
+                    )
+            else:
+                LOGGER.debug("Could not parse 'data' entries as structures.")
+                raise OPTIMADEParseError(
+                    "The DLite OPTIMADE Parser currently only supports structures entities."
+                )
+
+        # TODO: Deal with 'structures' variable
+
+        return session
