e8a76005682be94729b61bfcfc2577a7bd11ecfa
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2023-10-25 12:12:18 +0200

    Pass in config as a normal dict

--- tests/dlite/test_parse.py
+++ tests/dlite/test_parse.py
@@ -16,29 +16,26 @@ def test_parse(static_files: "Path", return_object: bool) -> None:
     from oteapi_dlite.utils import get_collection
 
     from oteapi_optimade.dlite.parse import OPTIMADEDLiteParseStrategy
-    from oteapi_optimade.models import OPTIMADEDLiteParseConfig
     from oteapi_optimade.models.custom_types import OPTIMADEUrl
 
     url = OPTIMADEUrl(
         "https://example.org/some/base/v0.1/optimade/v1/structures"
         '?filter=elements HAS ALL "Si","O"&sort=nelements&page_limit=2'
     )
-    config = OPTIMADEDLiteParseConfig(
-        **{
-            "mediaType": "application/vnd.OPTIMADE+DLite",
-            "downloadUrl": url,
-            "configuration": {
-                "datacache_config": {
-                    "expireTime": 60 * 60 * 24,
-                    "tag": "optimade",
-                    "accessKey": url,
-                },
-                "return_object": return_object,
+    config = {
+        "mediaType": "application/vnd.OPTIMADE+DLite",
+        "downloadUrl": url,
+        "configuration": {
+            "datacache_config": {
+                "expireTime": 60 * 60 * 24,
+                "tag": "optimade",
+                "accessKey": url,
             },
-        }
-    )
+            "return_object": return_object,
+        },
+    }
 
-    cache = DataCache(config.configuration.datacache_config)
+    cache = DataCache(config["configuration"]["datacache_config"])
     sample_file = static_files / "optimade_response.json"
     cache.add(
         {
